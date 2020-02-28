264c84db248c83517c8514ae768c5d5f5ac2f1c8
Edel-M <ser.nei.2819@gmail.com>
2020-02-28 16:38:21 -0700

    agregando carpeta layout-land

--- CONTROL_ROTACION_1/app/src/main/java/com/example/control_rotacion_1/MainActivity.java
+++ CONTROL_ROTACION_1/app/src/main/java/com/example/control_rotacion_1/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.control_rotacion_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+ImageView imgPetunia;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imgPetunia = findViewById(R.id.imgPetunia);
     }
 }
