90e986eae5b18f37a3cf87e5b60c7e9a1b8b416e
Edel-M <ser.nei.2819@gmail.com>
2020-03-01 00:57:42 -0700

    first commit

---
+++ CONTROL_ROTACION_12/app/src/main/java/com/example/control_rotacion_12/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.control_rotacion_12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+ImageView imgPetunia;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgPetunia = findViewById(R.id.imgPetunia);
+    }
+}
